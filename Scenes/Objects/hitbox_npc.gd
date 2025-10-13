9490b612f96b80acbb7a45bcc8dd7585538dad90
Christopher Barrett <mophydo@Chriss-Macbook-2.local>
2025-10-13 15:08:03 +1300

    Initial Commit

---
+++ Scenes/Objects/hitbox_npc.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body: Node) -> void:
+	if body.name == "PlayerFish":
+		print("The player fish touched the enemy fish!")
---
+++ Scripts/player_fish.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@export var score: int = 0
+@export var speed: float = 5.0  # Higher = faster follow
+@onready var sprite = $Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _process(delta: float) -> void:
+	
+	# Get mouse position and float towards it
+	var target = get_global_mouse_position()
+	position = position.lerp(target, speed * delta)
+	
+	# Flip sprite depending on position of mouse
+	var mouse_pos = get_global_mouse_position()
+	if mouse_pos.x < global_position.x:
+		sprite.flip_h = true   # face left
+	else:
+		sprite.flip_h = false  # face right
