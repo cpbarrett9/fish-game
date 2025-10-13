741c9509dbbd6c613360e0d6f9d5b116e2c36a16
Christopher Barrett <mophydo@Chriss-Macbook-2.local>
2025-10-14 11:37:32 +1300

    New spawning fish branch

--- Scripts/spawner.gd
+++ Scripts/spawner.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
-@export var xMin: int = 0
-@export var xMax: int = 3000
-@export var yMin: int = 0
-@export var yMax: int = 2000
+@export var xMin: int = -10
+@export var xMax: int = 3010
+@export var yMin: int = -10
+@export var yMax: int = 2010
+@export var minSeconds: float = 0
+@export var maxSeconds: float = 0.5
 @onready var fish_scene = preload("res://Scenes/Objects/npc_fish.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawn_fish(randf_range(1, 5)) # Spawns fish after 1-10 seconds (random)
+	spawn_fish(randf_range(minSeconds, maxSeconds)) # Spawns fish after 1-10 seconds (random)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,11 +18,23 @@ func _process(delta: float) -> void:
 
 func spawn_fish(seconds: float) -> void:
 	await get_tree().create_timer(seconds).timeout
+	var spawnPosition: Vector2
+	var travelSpeed: float
 	var spawnSide: int = randi_range(1, 2)
-	#match spawnSide:
-		#1: 
-	print("Spawned a fish")
 	var fish_instance = fish_scene.instantiate()  # Create a new copy of Fish.tscn
-	fish_instance.position = Vector2(randf_range(1, 1100), randf_range(1, 600))     # Set its position in world space
+	match spawnSide:
+		1: #LEFT:
+			spawnPosition = Vector2(xMin, randf_range(yMin, yMax)) # Spawn on left side
+			travelSpeed = 3 # Spawned on left, so travel right
+			fish_instance.get_node("Sprite2D").flip_h = false
+			print("Spawn on left")
+		2: #RIGHT:
+			spawnPosition = Vector2(xMax, randf_range(yMin, yMax)) # Spawn on right side
+			travelSpeed = -3 #Spawned on right, so travel left
+			fish_instance.get_node("Sprite2D").flip_h = true #flip the sprite if the fish is travelling left
+			print("Spawn on right")
+	print("Spawned a fish")
+	fish_instance.travelSpeed = travelSpeed
+	fish_instance.position = spawnPosition
 	add_child(fish_instance)                       # Add it to the current scene
-	spawn_fish(randf_range(1, 5)) # Spawns fish after 1-10 seconds (random)
+	spawn_fish(randf_range(minSeconds, maxSeconds)) # Spawns fish after 1-10 seconds (random)
