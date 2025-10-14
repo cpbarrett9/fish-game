e2ac8f429b3722c3231b3f621c39b3f42207877c
FoxtrotTwelve <n_e_painter@yahoo.com>
2025-10-14 13:53:06 +1300

    score and fish growth

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var score: int = 0
+var playerSize: int = 1
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,3 +12,22 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func getScore() -> int:
+	return score
+	
+func getSize() -> int:
+	return playerSize
+	
+func setSize(newSize: int) -> void:
+	playerSize = newSize
+
+func incrementSize() -> void:
+	playerSize = playerSize + 1
+
+
+signal score_changed(new_score)
+func set_score(value):
+	score = value
+	emit_signal("score_changed", score)
--- Scripts/player_fish.gd
+++ Scripts/player_fish.gd
@@ -23,3 +23,47 @@ func _process(delta: float) -> void:
 		sprite.flip_h = true   # face left
 	else:
 		sprite.flip_h = false  # face right
+	
+	var node_main: Node = get_node("/root/Main")
+	
+	
+	if node_main.getScore() >= 20 && node_main.getScore() < 60:
+		growSize(1.2, 1.2)
+		node_main.incrementSize
+	if node_main.getScore() >= 40:
+		growSize(1.4, 1.4)
+		node_main.incrementSize
+	if node_main.getScore() >= 60:
+		growSize(1.6, 1.6)
+		node_main.incrementSize
+	if node_main.getScore() >= 80:
+		growSize(1.8, 1.8)
+		node_main.incrementSize
+	if node_main.getScore() >= 100:
+		growSize(2, 2)
+		node_main.incrementSize
+	if node_main.getScore() >= 150:
+		growSize(2.3, 2.3)
+		node_main.incrementSize
+	if node_main.getScore() >= 200:
+		growSize(2.6, 2.6)
+		node_main.incrementSize
+	if node_main.getScore() >= 250:
+		growSize(3, 3)
+		node_main.incrementSize
+	#if node_main.getScore() >= 1000:
+		#growSize()
+		#node_main.incrementSize
+	#if node_main.getScore() >= 1000:
+		#growSize()
+		#node_main.incrementSize
+	#if node_main.getScore() >= 1000:
+		#growSize()
+		#node_main.incrementSize
+	#if node_main.getScore() >= 1000:
+		#growSize()
+		#node_main.incrementSize
+	
+	
+func growSize(x: float, y: float):
+	scale = Vector2(x, y)  # Doubles the size instantly
